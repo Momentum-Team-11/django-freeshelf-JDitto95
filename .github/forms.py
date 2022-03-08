bc6e3dbe6d38aa63916201e97830e1fd0c7fe4fa
Joseph Menditto <Powell0295@gmail.com>
2022-03-08 13:53:18 -0500

    working webpage

---
+++ .github/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from .models import Books
+
+
+#class (forms.ModelForm):
+    #class Meta:
+        #model = List_Books
+        #fields = [
+         #  'Title of book'
+          #  'Description'
+           # 'created_at'
+            
+        ]
--- books/models.py
+++ books/models.py
@@ -1,7 +1,13 @@
+from turtle import title
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    description = models.TextField()
+    created_at = models.DateTimeField( auto_now_add = True)
+    
 class User(AbstractUser):
     def __repr__(self):
         return f"<User username={self.username}>"
